fd2a951f8fc0b84a14c7c7368bd505516760d5d1
GSacata <gsacata@gmail.com>
2024-02-14 19:45:44 -0300

    1 created for loop

--- pergunta_1.py
+++ pergunta_1.py
@@ -1,6 +1,17 @@
 # v.0.0.0, 14/02/2024
-num_array = [2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
+# Status: Ongoing
 
-for index, item in num_array:
+num_array = [2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
+num_is_one = None
+
+for index, item in enumerate(num_array):
     if item == 1:
-        print("este é um número 1")
+        print(index, item) #DEV-ERASE
+        num_is_one = item
+        print(num_is_one) #DEV-ERASE
+        break
+    else:
+        continue
+
+print("fim") #DEV-ERASE
+
