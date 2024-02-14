b85e0ec6219ac18ba5c11b8aaf6f2819ea083825
GSacata <gsacata@gmail.com>
2024-02-14 19:58:56 -0300

    print ones

--- pergunta_1.py
+++ pergunta_1.py
@@ -2,16 +2,18 @@
 # Status: Ongoing
 
 num_array = [2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
-num_is_one = None
+num_one = None
+num_n_one = None
+
+
 
 for index, item in enumerate(num_array):
     if item == 1:
         print(index, item) #DEV-ERASE
-        num_is_one = item
-        print(num_is_one) #DEV-ERASE
-        break
     else:
         continue
+
+print(num_array)
 
 print("fim") #DEV-ERASE
 
