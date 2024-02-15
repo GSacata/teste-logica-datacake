ac6c5e0ff6bfa2c1e4a8c09279ee6441942cc7e3
GSacata <gsacata@gmail.com>
2024-02-14 21:03:50 -0300

    fix

--- pergunta_1.py
+++ pergunta_1.py
@@ -5,25 +5,20 @@ num_array = [2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
 num_one = None
 num_n_one = None
 
-def rearrange_item () :
-    print("rearrange_item")
-    # for index, item in enumerate(num_array):
-    #     if item != 1:
-    #         num_array.insert(index, 1)
-    #     else:
-    #         break
-    #     print(num_array)
-
-for index_out, item in enumerate(num_array):
-    if item == 1:
-        print(index_out, item) #DEV-ERASE
+for index_out, item_out in enumerate(num_array):
+    if item_out == 1:
+        print(index_out, item_out) #DEV-ERASE
+        num_one = num_array.pop(index_out)
         
-        for index_in, item in enumerate(num_array):
-            if item != 1:
+        for index_in, item_in in enumerate(num_array):
+            if item_in != 1:
                 num_n_one = num_array.pop(index_in)
                 print(num_n_one) #DEV-ERASE
-                num_array.insert(index_out, num_n_one)    
+                # num_array.insert(index_out, num_n_one)    
+                num_array.insert(index_out, num_n_one)
+                num_array.insert(0, num_one)
                 num_n_one = None
+                num_one = None
                 break
     else:
         continue
