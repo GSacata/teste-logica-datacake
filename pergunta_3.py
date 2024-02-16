5a0adf5a077d8579b9e7dec58a93cddbb7bc9921
GSacata <gsacata@gmail.com>
2024-02-15 21:23:16 -0300

    fixed function callings

--- pergunta_3.py
+++ pergunta_3.py
@@ -7,9 +7,10 @@
     # se nenhum da lista de pares existir, retorna false
 
 # x = input ("Digite um número inteiro: ")
-x = 8 #DEV-ER
+x = 3 #DEV-ER
 num_list = [1, 15, 2, 7, 2, 5, 7, 1, 4]
 sum_array = []
+total_sum_combo = []
 
 # Content: string filter, converting to int and etc.
 
@@ -18,15 +19,7 @@ new_num_list_1.sort()
 
 sum_biggest = new_num_list_1[-1] + new_num_list_1[-2]
 sum_smallest = new_num_list_1[0] + new_num_list_1[1]
-
-if x > sum_biggest:
-    print(f"No combinations available to numbers higher than {sum_biggest}")
-elif x <= sum_smallest:
-    print(f"No combinations available to numbers lower than {sum_smallest}")
-else:
-    print("x", x)
-    print("run search_combinations()")
-    
+   
 
 def create_sum_combo ():
     a, b = 0, x
@@ -36,23 +29,31 @@ def create_sum_combo ():
         invert_sum_pair = (b, a)
 
         if invert_sum_pair in sum_array:
-            print("pair already exists")
+            # pair already exists
+            pass
         else:
             sum_array.append(sum_pair)
         a += 1
         b -= 1
-    
-    print(sum_array)
-    print("end create_sum_combo ()")
-
-create_sum_combo ()
 
 def find_sum_combo ():
     for item in sum_array:
-        print(item) #DEV-ER
-        if item[0] and item[1] in num_list:
-            print(f"combination exist, {item}")
+        if item[0] in num_list and item[1] in num_list:
+            if item[0] == item[1] and num_list.count(item[0]) == 1:
+                pass # Repeated numbers, invalid pair
+            else:
+                total_sum_combo.append(item)
         else:
-            print(f"{item} combination does not exist")
+            pass # combination does not exist
 
-find_sum_combo ()
+
+if x > sum_biggest:
+    print(f"No combinations available to numbers higher than {sum_biggest}")
+elif x <= sum_smallest:
+    print(f"No combinations available to numbers lower than {sum_smallest}")
+else:
+    print("x =", x)
+    create_sum_combo ()
+    find_sum_combo ()
+
+print(f"Available combinations: {total_sum_combo}")
