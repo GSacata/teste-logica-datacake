19ce0ef6b0f97706f317c7e01b2ec2569736a301
GSacata <gsacata@gmail.com>
2024-02-16 20:54:29 -0300

    created num list filler

--- project-notes/pergunta_4.py
+++ project-notes/pergunta_4.py
@@ -1,11 +1,22 @@
 num_list = [9, 2, 3, 1, 4]
 initial_num_list = num_list.copy()
 missing_num_list = []
+interval_start = 0
+i = interval_start
 
-def find_biggest_num(list):
+def find_interval_end(list):
     list.sort()
     return list[-1]
 
-biggest_num = find_biggest_num(num_list)
+interval_end = find_interval_end(num_list)
 
-print(f"biggest_num, {biggest_num}") # DEV-ERASE
+while i < interval_end:
+    if i in num_list:
+        pass # 'i' number already exists in num_list
+    else:
+        missing_num_list.append(i)
+        num_list.append(i)
+    
+    i += 1
+
+print(f"Results:\nInitial num list: {initial_num_list},\nNumbers missing: {missing_num_list},\nCurrent num list: {num_list} ")
