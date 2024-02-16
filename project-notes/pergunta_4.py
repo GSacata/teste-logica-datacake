3edadd0b3b936774c84d6a5f69a8b535a62a423f
GSacata <gsacata@gmail.com>
2024-02-16 20:29:41 -0300

    created biggest number finder

--- project-notes/pergunta_4.py
+++ project-notes/pergunta_4.py
@@ -1,1 +1,11 @@
 num_list = [9, 2, 3, 1, 4]
+initial_num_list = num_list.copy()
+missing_num_list = []
+
+def find_biggest_num(list):
+    list.sort()
+    return list[-1]
+
+biggest_num = find_biggest_num(num_list)
+
+print(f"biggest_num, {biggest_num}") # DEV-ERASE
