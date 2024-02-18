060904c3a94005b3eec0e418b33884315535bb34
GSacata <gsacata@gmail.com>
2024-02-18 14:26:44 -0300

    creating node class methods

--- alt/pergunta_2_1.py
+++ alt/pergunta_2_1.py
@@ -2,11 +2,6 @@
 # Criada para treinar e exercitar árvore binária
 
 
-node_tree = None
-root_node = None
-# new_node_num = input("create int number node: ") # PROD-UNCOM
-new_node_num = 3 # DEV-ER
-
 class Node:
     def __init__(self, node_value):
         self.node_value = node_value
@@ -14,29 +9,16 @@ class Node:
         self.node_r_br = None
 
 
-def create_node(node_num):
-    node_num = int(node_num)
-    return Node(node_num)
+    def create_node(self, node_tree, node_num):
+        node_num = int(node_num)
 
-def append_node():
-    print("run append_node()")
-    
-    global root_node
+        if node_tree is None:
+            node_tree = self.__init__(self, node_num)
+        else:
+            print("Foi pra outro node")
+            pass
 
-    if root_node is None:
-        root_node = created_node
-    else:
-        print("appended in another node")
-
-def erase_created_node():
-    global created_node
-    created_node = None
-    
-created_node = create_node(new_node_num)
-print(vars(created_node))
-
-append_node()
-erase_created_node()
-
-print(created_node)
+root_node = Node(3)
 print(f"root_node: {vars(root_node)}")
+
+root_node.create_node(root_node, 5)
