5f8ffd2980d1d52b84651f2d1487288beeb393c9
GSacata <gsacata@gmail.com>
2024-02-18 14:41:34 -0300

    testing tree

--- alt/pergunta_2_1.py
+++ alt/pergunta_2_1.py
@@ -13,14 +13,19 @@ class Node:
         node_num = int(node_num)
 
         if node_tree is None:
-            node_tree = self.__init__(self, node_num)
+            # node_tree = self.__init__(self, node_num)
+            node_tree = self.__init__(node_num)
         else:
             print("Vai para outro node")
 
             if node_num > self.node_value:
                 print("vai para a direita") #DEV-ER
+                # self.node_r_br = self.__init__(node_num)
+                self.node_r_br = self.create_node(self.node_r_br, node_num)
             else:
                 print("vai para a esquerda") #DEV-ER
+                # self.node_l_br = self.__init__(node_num)
+                self.node_l_br = self.create_node(self.node_l_br, node_num)
             
 
 root_node = Node(3)
@@ -28,3 +33,5 @@ print(f"root_node: {vars(root_node)}")
 
 root_node.create_node(root_node, 5)
 root_node.create_node(root_node, 2)
+
+print(f"root_node: {vars(root_node)}")
