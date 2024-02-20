629b6e533f913cbe25593cbc4139a0f95e0ea15b
GSacata <gsacata@gmail.com>
2024-02-20 20:37:51 -0300

    remodelling tree

--- pergunta_2.py
+++ pergunta_2.py
@@ -35,3 +35,32 @@ tree = {
         }
     }
 }
+
+text_input = input("What word to find? ")
+
+def format_input():
+    print("Formatting the input...")
+
+def search_tree():
+    print("Searching the tree...")
+
+
+test_obj = {
+    "value": "", 
+    "l_br": {},
+    "l_flag": 1,
+    "r_br": {},
+    "r_flag": 1
+    }
+
+class Tree():
+    def __init__(self, tree):
+        self.tree = tree
+    
+    def hello_tree(self):
+        print("Hello to the trees of the world")
+
+new_tree = Tree(tree)
+
+print(vars(new_tree))
+new_tree.hello_tree()
