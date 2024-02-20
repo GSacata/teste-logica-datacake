53b07e5914d0727dbc8f3013a693d69f68363a7b
GSacata <gsacata@gmail.com>
2024-02-20 20:07:44 -0300

    starting tree created

---
+++ pergunta_2.py
@@ -0,0 +1,37 @@
+tree = {
+    "value": "Maçã",
+    "left_br": {
+        "value": "Morango", 
+        "left_br": {
+            "value": "Goiaba", 
+            "left_br": {},
+            "right_br": {}
+        },
+        "right_br": {
+            "value": "Limão", 
+            "left_br": {},
+            "right_br": {}
+        }
+    },
+    "right_br": {
+        "value": "Pera", 
+        "left_br": {},
+        "right_br": {
+            "value": "Abacaxi", 
+            "left_br": {},
+            "right_br": {
+                "value": "Laranja", 
+                "left_br": {
+                    "value": "Banana", 
+                    "left_br": {},
+                    "right_br": {}
+                },
+                "right_br": {
+                    "value": "Cebola", 
+                    "left_br": {},
+                    "right_br": {}
+                }
+            }
+        }
+    }
+}
