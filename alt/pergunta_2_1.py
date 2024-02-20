54f09bf03d3c84ae6bad4500227d12baca6f2463
GSacata <gsacata@gmail.com>
2024-02-19 21:38:43 -0300

    finally a working one

--- alt/pergunta_2_1.py
+++ alt/pergunta_2_1.py
@@ -2,55 +2,34 @@
 # Criada para treinar e exercitar árvore binária
 
 
-# class Node:
-#     def __init__(self, node_value):
-#         self.node_value = node_value
-#         self.node_l_br = None
-#         self.node_r_br = None
-
-
-#     def create_node(self, node_tree, node_num):
-#         node_num = int(node_num)
-
-#         if node_tree is None:
-#             # node_tree = self.__init__(self, node_num)
-#             node_tree = self.__init__(node_num)
-#         else:
-#             print("Vai para outro node")
-
-#             if node_num > self.node_value:
-#                 print("vai para a direita") #DEV-ER
-#                 # self.node_r_br = self.__init__(node_num)
-#                 self.node_r_br = self.__init__(self, node_tree, node_num)
-#             else:
-#                 print("vai para a esquerda") #DEV-ER
-#                 # self.node_l_br = self.__init__(node_num)
-#                 self.node_l_br = self.__init__(self, node_tree, node_num)
-            
-
-# root_node = Node(3)
-# print(f"root_node: {vars(root_node)}")
-
-# root_node.create_node(root_node, 5)
-# root_node.create_node(root_node, 2)
-
-# print(f"root_node: {vars(root_node)}")
-
+print_line = []
 
 class Node():
-    def __init__(self):
-        return None
+    def __init__(self, node_value):
+        self.value = node_value
+        self.left_br = None
+        self.right_br = None
     
-    def fill_node(self, node_obj, node_value):
-        if 0 == 1:
-            pass
+    def create_node(self, node_value):
+        if self.value:
+            if node_value > self.value:
+                if self.right_br is None:
+                    self.right_br = Node(node_value)
+                else:
+                    self.right_br.create_node(node_value)
+            
+            elif node_value < self.value:
+                if self.left_br is None:
+                    self.left_br = Node(node_value)
+                else:
+                    self.left_br.create_node(node_value)
+
         else:
-            node_obj.value = node_value
-            node_obj.left_br = {}
-            node_obj.right_br = {}
+            self.value = node_value
 
-tree = Node()
+
+tree = Node(2)
 print(vars(tree))
 
-tree.fill_node(tree, 3)
-print(vars(tree))
+tree.create_node(3)
+tree.create_node(5)
